972ef5d68654f8d299d0e4cbd04337ebb34cefc2
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2022-04-05 18:34:06 -0700

    allow requests from netlify preview deploys

--- api/settings.py
+++ api/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = "django-insecure-ti9c^1v_vc^0-kw35z!!-0a=_n6=fe8#q(5h%0*z!6b&n*n!(b
 # DEBUG = True
 DEBUG = False
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = [
-  "http://localhost:3000",
-  "https://ubc-coursecrawler.netlify.app"
-]
+ALLOWED_HOSTS = ["*"]
 
 # Redis Database
 REDIS_HOST = "redis-12422.c241.us-east-1-4.ec2.cloud.redislabs.com"
@@ -63,10 +59,10 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-  "http://localhost:3000",
-  "https://ubc-coursecrawler.netlify.app"
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = (
+  "http:\/\/localhost:3000",
+  "https:\/\/.*ubc-coursecrawler.netlify.app"
 )
 ROOT_URLCONF = "api.urls"
 
